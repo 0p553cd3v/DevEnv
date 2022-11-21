67d3497df3e49eec5dbfdd80cb733dde117432a5
0p553cd3v <0p553cd3v@gmail.com>
2022-11-21 22:25:06 +0100

    [DDE-2] Cleaning build cache before build

--- build/build.sh
+++ build/build.sh
@@ -8,6 +8,6 @@ Log_Open
 
 #devenv docker image build
 cd $DDE_CONF_DIR/ubuntu/
-run_func 0 "sudo docker build -t devenv -f $DDE_CONF_DIR/ubuntu/Dockerfile ."
+run_func 0 "sudo docker build -t devenv -f $DDE_CONF_DIR/ubuntu/Dockerfile --no-cache ."
 
 Log_Close
