a8f925a7a62da857e1063b73b54533ca5b149766
0p553cd3v <0p553cd3v@gmail.com>
2022-11-21 21:17:14 +0100

    [DDE-2] Correcting to long path for copy dockefile

--- tools/def_setup.sh
+++ tools/def_setup.sh
@@ -54,7 +54,7 @@ create_dir "$DDE_DCV_DIR"
 create_dir "$DDE_DCV_DIR/ubuntu"
 
 copy_file $DDE_BLD_DIR/docker-compose.yaml $DDE_CONF_DIR
-copy_file $DDE_SRC_DIR/ubuntu/Dockerfile $DDE_CONF_DIR/devenv/ubuntu
+copy_file $DDE_SRC_DIR/ubuntu/Dockerfile $DDE_CONF_DIR/ubuntu
 copy_file $DDE_REPO_DIR/config/ubuntu/x11vnc.service $DDE_CONF_DIR/ubuntu
 
 if [[ $1 -eq null ]]; then
