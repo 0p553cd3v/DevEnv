400758db90c0d661b9c3a80fa17c7986c2767808
0p553cd3v <0p553cd3v@gmail.com>
2022-11-25 21:06:13 +0100

    [DDE-2] Change of build sequance

--- build/build.sh
+++ build/build.sh
@@ -8,6 +8,6 @@ Log_Open
 
 #devenv docker image build
 cd $DDE_CONF_DIR/ubuntu/
-run_func 0 "sudo docker build -t devenv -f $DDE_CONF_DIR/ubuntu/Dockerfile --no-cache ."
+run_func 0 "sudo docker build -t devenv -f $DDE_CONF_DIR/ubuntu/Dockerfile ."
 
 Log_Close
