22350c51c7875580095bec13a57123733541085b
0p553cd3v <0p553cd3v@gmail.com>
2022-11-27 23:01:01 +0100

    [DDE-3] entrypoint script correction

--- src/ubuntu/entrypoint.sh
+++ src/ubuntu/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 service dbus start 
-while [ "$(/bin/bash/service dbus status | grep 'is running')" ]; do
+while [ -z "$(service dbus  status | grep 'is running')" ]; do
     sleep 1
     echo -e "Waiting fo dbus \n"
 done
 echo "DBUS service started"
 
 service lightdm start
-while [ "$(/bin/bash/service lightdm status | grep 'is running')" ]; do
+while [ -z "$(service lightdm  status | grep 'is running')" ]; do
     sleep 1
     echo -e "Waiting for lightdm \n"
 done
